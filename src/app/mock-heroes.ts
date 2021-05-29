d0fe81155fc0692e2ebfe844ec1c9764a39d0bd7
ashrafulais <ashraful0103@gmail.com>
2021-05-29 23:34:52 +0600

    List showing and styling

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Hitler 🤯' },
+  { id: 12, name: 'Durian man 🍄' },
+  { id: 13, name: 'La tomato 🍅' },
+  { id: 14, name: 'Sleepy pup 🐶' },
+  { id: 15, name: 'Night baby 😴' },
+  { id: 16, name: 'Lipstick 💄' },
+  { id: 17, name: 'Grumpy cat 😾' },
+  { id: 18, name: 'Hungry Tummy 🥙' },
+  { id: 19, name: 'Vice city police 🕵️‍♀️' },
+  { id: 20, name: 'You 😁' }
+];
