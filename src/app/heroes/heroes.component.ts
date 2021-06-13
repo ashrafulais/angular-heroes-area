8f7c73412be2fd574742d936ad3a94fe56539655
ashrafulais <ashraful0103@gmail.com>
2021-06-13 12:40:51 +0600

    variable name changed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,6 +20,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+    this.heroService
+      .getHeroes()
+      .subscribe(heroesList => (this.heroes = heroesList));
   }
 }
